ed4af1a33bf715c6c2f48eb30f98aa2a466b9728
Carlos <clockwerkz@gmail.com>
2017-05-25 11:45:59 -0700

    Getting basic functionality to work. Added in digit length detection, and determining if the keypress should add a number or replce the zero number display.

--- js/jsCalculator.js
+++ js/jsCalculator.js
@@ -1,8 +1,27 @@
 var calcButtons = document.querySelectorAll(".button");
 var display = document.getElementById("display");
+var secondDisplay = document.getElementById("accumulative");
+var isOperator = false;
 
 calcButtons.forEach(function(button){
 	button.addEventListener("click", function(){
-		display.textContent += this.dataset.value;
+		var firstNum, secondNum;
+		var val = this.dataset.value;
+		if ((Number(val) >= 0) && (Number(val) <= 9)){
+			//First check and see if the number displayed is 0. If it is, then replace 0 with value.
+			//If not zero, then append the value to existing number.
+			if (display.textContent === "0"){
+				display.textContent = val;
+			} else {
+				if (display.textContent.length === 9){
+					secondDisplay.textContent = "Digit Limit";
+					display.textContent = 0;
+				} else {
+					secondDisplay.textContent = "";
+					display.textContent += val;
+				}
+			}
+		}
+		if (val) 
 	});
 });
